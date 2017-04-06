096017639870b2c3fafd664ceed28aa6f34a476f
Andy Feng <anndyfeng1@gmail.com>
2017-04-06 10:19:18 -0700

    Initial Commit

---
+++ thursday_demo_ios_0417/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thursday_demo_ios_0417
+//
+//  Created by Andy Feng on 4/6/17.
+//  Copyright © 2017 Andy Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
